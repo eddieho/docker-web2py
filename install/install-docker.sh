1c33ca68e882731b911eb7d39db944ea9fc29e21
Ubuntu-16-server-x64-base-01 <dev@ubuntu16-dev01.devdomain>
2017-07-28 18:04:15 +1000

    initial load to test git repo

---
+++ install/install-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Docker CE for Ubuntu installation is https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#os-requirements
+# need to automate the steps by script
+
+echo "TBD"
