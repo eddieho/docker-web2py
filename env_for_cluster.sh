ba70a9856169268fa941c01efe43459af6cdafae
Eddie <eddieho@users.noreply.github.com>
2017-08-02 16:27:22 +1000

    use environment variables for image name in docker_compose_cluster.yml

--- env_for_cluster.sh
+++ env_for_cluster.sh
@@ -10,8 +10,14 @@ export REGISTRY_CONTAINER_PORT=5000
 # web2py - apache2
 export APACHE2_CONF_DIR=$HOME/web2py/apache2/conf
 export APACHE2_HTDOCS_DIR=$HOME/web2py/apache2/htdocs
+export WEB2PY_APACHE2_IMAGE=${REGISTRY_HOSTNAME}:${REGISTRY_PUBLISHED_PORT}/web2py_apache2
+
+# web2py - python27 
+export WEB2PY_PYTHON27_IMAGE=${REGISTRY_HOSTNAME}:${REGISTRY_PUBLISHED_PORT}/web2py_python27
 
 # web2py - MySQL 
 export MYSQL_CONF_DIR=$HOME/web2py/mysql5/mysql_cnf/mysql.conf.d
 export MYSQL_DATA_DIR=$HOME/web2py/mysql5/datadir
 export MYSQL_INIT_DIR=$HOME/web2py/mysql5/init
+export WEB2PY_MYSQL5_IMAGE=${REGISTRY_HOSTNAME}:${REGISTRY_PUBLISHED_PORT}/web2py_mysql5
+
